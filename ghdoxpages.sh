8290212432fab6998fd9357e9f96b491e5178aa9
Dan Fekete <thefekete@gmail.com>
2015-03-14 13:57:41 +0100

    fixes if statement

--- ghdoxpages.sh
+++ ghdoxpages.sh
@@ -4,12 +4,9 @@
 ## \file
 
 # make sure we have a clean tree and index
-git diff --quiet --exit-code && git diff --cached --quiet --exit-code
-if [ $? ]; then
+if git diff --quiet --exit-code && git diff --cached --quiet --exit-code; then
     echo "Working tree and index are clean. Proceeding"
 else
     >&2 echo "Working tree or index is dirty, exiting!"
     exit 1
 fi
-
-echo "Hello World"
